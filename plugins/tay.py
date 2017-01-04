1de56c3436a1519698facea15e306791ec9ab472
Anil Kulkarni <anil@textio.com>
2017-01-04 07:30:19 -0800

    Spew lyrics every 5 minutes

--- plugins/tay.py
+++ plugins/tay.py
@@ -1,5 +1,6 @@
-from rtmbot.core import Plugin
+from rtmbot.core import Plugin, Job
 import os
+import random
 
 bot_id = 'U3MSN806S'
 header = '<@{}> '.format(bot_id)
@@ -12,26 +13,42 @@ responses = {
 directory = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-lyrics = []
+all_lyrics = []
 with open(os.path.join(directory, 'lyrics.txt')) as fp:
     cur = ''
     for line in fp:
         if line.strip():
             cur += line
         else:
-            lyrics.append(cur.strip())
+            all_lyrics.append(cur.strip())
             cur = ''
 
+class LyricJob(Job):
+
+    def run(self, slack_client):
+        content = random.choice(all_lyrics)
+        channels = slack_client.api_call('channels.list')['channels']
+        channel_id = None
+        for channel in channels:
+            if channel['name'] == 'aoeu':
+                channel_id = channel['id']
+                break
+
+        return [
+            [channel_id, "You are listening to :tay: radio. Don't forget to #votefortay"],
+            [channel_id, content],
+        ]
 
 class Tay(Plugin):
 
+    def register_jobs(self):
+        job = LyricJob(60 * 5)
+        self.jobs.append(job)
+
     def process_message(self, data):
-        print(data)
         if data['text'].startswith(header):
             message = data['text'][len(header):].strip().lower()
-            print('winner winner {}'.format(message))
             if responses[message]:
                 response = responses[message]
-                print('chicken dinner {}'.format(response))
                 self.outputs.append([data['channel'], response])
                 self.outputs.append([data['channel'], ":tay::tay::tay:Don't forget to #votefortay :tay::tay::tay:"])
