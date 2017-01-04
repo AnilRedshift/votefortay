0e46d1c7d6ebea54f44171ee3e4b5471514e35e9
Anil Kulkarni <anil@textio.com>
2017-01-04 06:57:08 -0800

    Initial plugin

---
+++ plugins/tay.py
@@ -0,0 +1,20 @@
+from rtmbot.core import Plugin
+
+bot_id = 'U3MSN806S'
+header = '<@{}> '.format(bot_id)
+
+responses = {
+    'hi': 'Nice to meet you, where you been?',
+}
+
+class Tay(Plugin):
+
+    def process_message(self, data):
+        print(data)
+        if data['text'].startswith(header):
+            message = data['text'][len(header):].strip().lower()
+            print('winner winner {}'.format(message))
+            if responses[message]:
+                response = responses[message]
+                print('chicken dinner {}'.format(response))
+                self.outputs.append([data['channel'], response])
