7b18c4c87b162cbd8cbad724f9dbb1a505f473c8
Anil Kulkarni <anil@textio.com>
2017-01-04 08:20:39 -0800

    update send.py

--- send.py
+++ send.py
@@ -2,9 +2,12 @@ from slackclient import SlackClient
 import os
 import sys
 
-def send(message):
+def send(message, channel):
     sc = SlackClient(os.environ['SLACK_TOKEN'])
-    sc.api_call('chat.postMessage', channel='#aoeu', text=message, as_user=True)
+    sc.api_call('chat.postMessage', channel=channel, text=message, as_user=True)
 
 if __name__ == '__main__':
-    send(sys.argv[1])
+    channel = '#emoji'
+    if len(sys.argv) >= 3:
+        channel = sys.argv[2]
+    send(sys.argv[1], channel)
